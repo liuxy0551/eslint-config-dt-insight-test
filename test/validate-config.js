5abfab6228c403bb0a52337c3ab424f830db6a38
liuyi <liuyi@dtstack.com>
2022-04-06 21:09:50 +0800

    test 3

--- test/validate-config.js
+++ test/validate-config.js
@@ -3,7 +3,7 @@ const test = require('tape')
 
 test('load config in eslint to validate all rule syntax is correct', async function (t) {
     const eslint = new ESLint()
-    const code = 'const foo = 1\nconst bar = function () {}\nbar(foo)\n'
+    const code = 'const foo = 1\nconst bar = (val) => val + 1\nbar(foo)\n'
     const [lintResult] = await eslint.lintText(code)
     t.equal(lintResult.errorCount, 0)
     t.end()
@@ -11,7 +11,7 @@ test('load config in eslint to validate all rule syntax is correct', async funct
 
 test('ensure we allow top level await', async function (t) {
     const eslint = new ESLint()
-    const code = 'const foo = await 1\nconst bar = function () {}\nawait bar(foo)\n'
+    const code = 'const foo = await 1\nconst bar = (val) => val + 2\nawait bar(foo)\n'
     const [lintResult] = await eslint.lintText(code)
     t.equal(lintResult.errorCount, 0)
     t.end()
