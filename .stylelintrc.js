9250b98b3a432d2c3eaf8764eeb06c260290c0d4
Liuxy0551 <liuxy0551@qq.com>
2022-04-27 20:46:58 +0800

    Develop (#22)
    * docs: readme
    
    * feat: no-console 0
    
    * feat: add stylelint
    
    * feat: stylelint rules
    
    * feat: new rules
    
    Co-authored-by: liuyi <liuyi@dtstack.com>

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,5 +1,13 @@
 module.exports = {
     extends: 'stylelint-config-standard',
     plugins: ['stylelint-order', 'stylelint-scss'],
+    // customSyntax: 'postcss-scss',
     files: ['**/*.css', '**/*.less', '**/*.scss', '**/*.sass'],
+    rules: {
+        indentation: 4,
+        'declaration-empty-line-before': 'never',
+        'rule-empty-line-before': 'never',
+        // 'selector-class-pattern': 'ant-[a-z]+',
+
+    },
 };
