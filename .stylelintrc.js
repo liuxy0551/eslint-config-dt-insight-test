7f4d7fe5a344d88535526b7e95828b5908bf9acb
Liuxy0551 <liuxy0551@qq.com>
2022-04-28 11:55:43 +0800

    Develop (#24)
    * docs: readme
    
    * feat: no-console 0
    
    * feat: add stylelint
    
    * feat: stylelint rules
    
    * feat: new rules
    
    * feat: off react-hooks/exhaustive-deps
    
    * perf: add postcss for post-scss
    
    Co-authored-by: liuyi <liuyi@dtstack.com>

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,13 +1,21 @@
 module.exports = {
+    files: ['**/*.css', '**/*.scss'],
+    customSyntax: require('postcss-scss'),
     extends: 'stylelint-config-standard',
     plugins: ['stylelint-order', 'stylelint-scss'],
-    // customSyntax: 'postcss-scss',
-    files: ['**/*.css', '**/*.less', '**/*.scss', '**/*.sass'],
     rules: {
-        indentation: 4,
-        'declaration-empty-line-before': 'never',
-        'rule-empty-line-before': 'never',
+        indentation: 4, // 缩进4格
+        'declaration-empty-line-before': 'never', // 第一条属性声明前不允许有空行
         // 'selector-class-pattern': 'ant-[a-z]+',
 
+        // 规则之前的空行
+        'rule-empty-line-before': [
+            'always',
+            {
+                except: ['inside-block', 'first-nested'],
+            },
+        ],
+        'alpha-value-notation': 'number', // 小数显示数字(number)或百分数(percentage)
+        'color-function-notation': 'legacy', // 颜色 rgba 等使用传统逗号隔开
     },
 };
